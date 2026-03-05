06b150fc4867b1d61796bb6b1ecab80b54d1d566
jsolorzanoc <jsolorzanoc08@outlook.com>
2026-03-05 12:47:38 -0600

    Cell class created

--- MineSweeperV1/src/minesweeper/Cell.java
+++ MineSweeperV1/src/minesweeper/Cell.java
@@ -2,6 +2,52 @@ package minesweeper;
 
 public class Cell {
 
+    private boolean isMine;
+    private boolean isFlagged;
+    private boolean isRevealed;
+    private int adjacentMines; //Nearby mines
+
+    public Cell(){ //Game start defaults no paramethers
+        this.adjacentMines = 0;
+        this.isFlagged = false;
+        this.isMine = false;
+         this.isRevealed = false;
+    }
+
+    public boolean isMine() {
+        return isMine;
+    }
+
+    public void setMine(boolean mine) {
+        isMine = mine;
+    }
+
+    public boolean isFlagged() {
+        return isFlagged;
+    }
+
+
+    public int getAdjacentMines() {
+        return adjacentMines;
+    }
+
+    public void setAdjacentMines(int adjacentMines) {
+        this.adjacentMines = adjacentMines;
+    }
+
+    public boolean isRevealed() {
+        return isRevealed;
+    }
+
+    public void reveal(){
+        this.isRevealed = true;
+    }
+
+    public void toggleFlag(){
+        this.isFlagged = !this.isFlagged;
+    }
+
+
 
 
 
