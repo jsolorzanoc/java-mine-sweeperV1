d9d81225e7527d664f0b92d6424107dfb97145bf
jsolorzanoc <jsolorzanoc08@outlook.com>
2026-03-06 08:40:45 -0600

    Board class progress

--- MineSweeperV1/src/minesweeper/Board.java
+++ MineSweeperV1/src/minesweeper/Board.java
@@ -1,4 +1,88 @@
 package minesweeper;
 
+import java.util.Random;
+
 public class Board {
+    private Cell[][] grid; // 2d array that holds a cell object.
+    private int rows;
+    private int cols;
+    private int mines;
+
+
+    public Board(){  //Constructor overload
+        this (9,9,8);
+    }
+
+    public Board(int rows, int  cols, int mines){
+        this.cols = cols;
+        this.rows = rows;
+        this.mines = mines;
+        this.grid = new Cell[cols][rows];
+    }
+
+    public void initCells(){ // Poblates cells with a new cell obj
+        for (var i = 0; i < rows; i++){
+            for (var j = 0; j < cols; j++){
+                grid[i][j] = new Cell();
+            }
+        }
+    }
+
+    public void placeMines(){
+        Random rand = new Random();
+        int minesPlaced = 0;
+        while (minesPlaced < mines){
+            int r = rand.nextInt(rows);
+            int c = rand.nextInt(cols);
+            if (!grid[r][c].isMine()){
+                grid[r][c].setMine(true);
+                minesPlaced++;
+            }
+        }
+
+    }
+
+    public void printGrid(){
+        for (var i = 0; i < rows; i++){
+            for (var j = 0; j < cols; j++){
+                String value = grid[i][j].isMine() ?  "M " :
+                        grid[i][j].getAdjacentMines() > 0 ?  grid[i][j].getAdjacentMines() + " " :
+                                ". ";
+
+                System.out.print(value);
+            }
+            System.out.println();
+        }
+    }
+
+    public void calculateAdjacentMines(){
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+
+                if (grid[i][j].isMine()) continue;
+
+            int count = 0;
+
+            for (int dr = -1; dr <= 1; dr++) {
+                for (int dc = -1; dc <= 1; dc++){
+
+                    if (dr == 0 && dc == 0) continue;
+
+                    int nr = i + dr; //Vecino xy
+                    int nc = j + dc; //Vecino xy
+
+                    if (nr >= 0 && nr < rows && nc >= 0 && nc< cols){
+                        if (grid[nr][nc].isMine()) count++;
+                    }
+                }
+            }
+                grid[i][j].setAdjacentMines(count);
+        }
+
+    }
+
+
+    }
+
+
 }
---
+++ MineSweeperV1/src/minesweeper/Main.java
@@ -0,0 +1,12 @@
+package minesweeper;
+
+public class Main {
+    public static void main(String[] args) {
+        var cell1 = new Cell();
+        var board1 = new Board();
+        board1.initCells();
+        board1.placeMines();
+        board1.calculateAdjacentMines();
+        board1.printGrid();
+    }
+}
